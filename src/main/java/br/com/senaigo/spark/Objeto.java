da42da42690c53fa3ac6f5ef8ba0a84096117c92
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-04-05 22:24:29 -0300

    spark java

---
+++ src/main/java/br/com/senaigo/spark/Objeto.java
@@ -0,0 +1,15 @@
+package br.com.senaigo.spark;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Objeto {
+	private Integer id;
+	private Integer userId;
+	private String title;
+	private String body;
+}
